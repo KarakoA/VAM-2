15329b28f4d5c0bcbb50fcd6478bfbd643dc4ba4
Anton K <s0553324@htw-berlin.de>
2020-09-12 14:57:06 +0300

    simplify trainer

--- src/train/trainer.py
+++ src/train/trainer.py
@@ -55,23 +55,16 @@ class Trainer:
 
         # misc params
         self.best = config.best
-        self.ckpt_dir = config.ckpt_dir
-        self.logs_dir = config.logs_dir
         self.best_valid_acc = 0.0
         self.counter = 0
-        self.train_patience = config.train_patience
-        self.use_tensorboard = config.use_tensorboard
-        self.print_freq = config.print_freq
-        self.plot_freq = config.plot_freq
-        self.model_name = config.model_name
 
-        self.plot_dir = "./plots/" + self.model_name + "/"
+        self.plot_dir = "./plots/" + self.config.model_name + "/"
         if not os.path.exists(self.plot_dir):
             os.makedirs(self.plot_dir)
 
         # configure tensorboard logging
-        if self.use_tensorboard:
-            tensorboard_dir = self.logs_dir + self.model_name
+        if self.config.use_tensorboard:
+            tensorboard_dir = self.config.logs_dir + self.config.model_name
             print("[*] Saving tensorboard logs to {}".format(tensorboard_dir))
             if not os.path.exists(tensorboard_dir):
                 os.makedirs(tensorboard_dir)
@@ -129,7 +122,7 @@ class Trainer:
             # check for improvement
             if not is_best:
                 self.counter += 1
-            if self.counter > self.train_patience:
+            if self.counter > self.config.train_patience:
                 print("[!] No improvement in a while, stopping training.")
                 return
             self.best_valid_acc = max(valid_acc, self.best_valid_acc)
@@ -159,7 +152,7 @@ class Trainer:
         with tqdm(total=self.num_train) as pbar:
             for i, (x, y) in enumerate(self.train_loader):
                 self.optimizer.zero_grad()
-                loss, acc, preds, locs, imgs = self.one_epoch(x, y)
+                loss, acc, preds, locs, imgs = self.one_batch(x, y)
 
                 # compute gradients and update SGD
                 loss.backward()
@@ -182,15 +175,14 @@ class Trainer:
                 self.__save_images_if_plotting(epoch, i, locs, imgs)
 
                 # log to tensorboard
-                if self.use_tensorboard:
+                if self.config.use_tensorboard:
                     iteration = epoch * len(self.train_loader) + i
                     log_value("train_loss", losses.avg, iteration)
                     log_value("train_acc", accs.avg, iteration)
 
             return losses.avg, accs.avg
 
-    def one_epoch(self, x, y):
-
+    def one_batch(self, x, y):
         # initialize location vector and hidden state
         batch_size = x.shape[0]
         x, y = x.to(self.device), y.to(self.device)
@@ -250,7 +242,7 @@ class Trainer:
 
     def __save_images_if_plotting(self, epoch, i, locs, imgs):
         # dump the glimpses and locs
-        if (epoch % self.plot_freq == 0) and (i == 0):
+        if (epoch % self.config.plot_freq == 0) and (i == 0):
             imgs = [g.cpu().data.numpy().squeeze() for g in imgs]
             locs = [l.cpu().data.numpy() for l in locs]
             pickle.dump(imgs, open(self.plot_dir + "g_{}.p".format(epoch + 1), "wb"))
@@ -262,74 +254,17 @@ class Trainer:
         """
         losses = AverageMeter()
         accs = AverageMeter()
-
+        # TODO check
+        self.model.test()
         for i, (x, y) in enumerate(self.valid_loader):
-            x, y = x.to(self.device), y.to(self.device)
-
-            # initialize location vector and hidden state
-            batch_size = x.shape[0]
-            l_t = self.model.reset(batch_size, self.device)
-
-            # extract the glimpses
-            log_pi = []
-            baselines = []
-            for t in range(self.num_glimpses - 1):
-                # forward pass through model
-                h_t, l_t, b_t, p = self.model(x, l_t)
-
-                # store
-                baselines.append(b_t)
-                log_pi.append(p)
-
-            # last iteration
-            h_t, l_t, b_t, log_probas, p = self.model(x, l_t, last=True)
-            log_pi.append(p)
-            baselines.append(b_t)
-
-            # convert list to tensors and reshape
-            baselines = torch.stack(baselines).transpose(1, 0)
-            log_pi = torch.stack(log_pi).transpose(1, 0)
-
-            # average
-            # M
-            log_probas = log_probas.view(1, -1, log_probas.shape[-1])
-            log_probas = torch.mean(log_probas, dim=0)
-
-            # M
-            baselines = baselines.contiguous().view(1, -1, baselines.shape[-1])
-            baselines = torch.mean(baselines, dim=0)
-
-            # M
-            log_pi = log_pi.contiguous().view(1, -1, log_pi.shape[-1])
-            log_pi = torch.mean(log_pi, dim=0)
-
-            # calculate reward
-            predicted = torch.max(log_probas, 1)[1]
-            R = (predicted.detach() == y).float()
-            R = R.unsqueeze(1).repeat(1, self.num_glimpses)
-
-            # compute losses for differentiable modules
-            loss_action = F.nll_loss(log_probas, y)
-            loss_baseline = F.mse_loss(baselines, R)
-
-            # compute reinforce loss
-            adjusted_reward = R - baselines.detach()
-            loss_reinforce = torch.sum(-log_pi * adjusted_reward, dim=1)
-            loss_reinforce = torch.mean(loss_reinforce, dim=0)
-
-            # sum up into a hybrid loss
-            loss = loss_action + loss_baseline + loss_reinforce * 0.01
-
-            # compute accuracy
-            correct = (predicted == y).float()
-            acc = 100 * (correct.sum() / len(y))
+            loss, acc, preds, locs, imgs = self.one_batch(x, y)
 
             # store
             losses.update(loss.item(), x.size()[0])
             accs.update(acc.item(), x.size()[0])
 
             # log to tensorboard
-            if self.use_tensorboard:
+            if self.config.use_tensorboard:
                 iteration = epoch * len(self.valid_loader) + i
                 log_value("valid_loss", losses.avg, iteration)
                 log_value("valid_acc", accs.avg, iteration)
@@ -338,45 +273,28 @@ class Trainer:
 
     @torch.no_grad()
     def test(self):
-        """Test the RAM model.
-
-        This function should only be called at the very
-        end once the model has finished training.
+        """
+        Test the RAM model.
         """
         correct = 0
         preds = []
+
         # load the best checkpoint
         self.load_checkpoint(best=self.best)
+        # TODO check
+        self.model.test()
 
         for i, (x, y) in enumerate(self.test_loader):
-            x, y = x.to(self.device), y.to(self.device)
+            loss, acc, predictions, locs, imgs = self.one_batch(x, y)
 
-            # initialize location vector and hidden state
-            batch_size = x.shape[0]
-            l_t = self.model.reset(batch_size, self.device)
-
-            # extract the glimpses
-            for t in range(self.num_glimpses - 1):
-                # forward pass through model
-                h_t, l_t, b_t, p = self.model(x, l_t)
-
-            # last iteration
-            h_t, l_t, b_t, log_probas, p = self.model(x, l_t, last=True)
-
-            # M
-            log_probas = log_probas.view(1, -1, log_probas.shape[-1])
-            log_probas = torch.mean(log_probas, dim=0)
-
-            pred = log_probas.data.max(1, keepdim=True)[1]
-            correct += pred.eq(y.data.view_as(pred)).cpu().sum()
-            preds.append(pred)
+            # TODO check
+            correct += (predictions == y).float()
+            preds.append(predictions)
         perc = (100.0 * correct) / (self.num_test)
         error = 100 - perc
-        print(
-            "[*] Test Acc: {}/{} ({:.2f}% - {:.2f}%)".format(
-                correct, self.num_test, perc, error
-            )
-        )
+
+        print("[*] Test Acc: {}/{} ({:.2f}% - {:.2f}%)".format(
+            correct, self.num_test, perc, error))
         return preds
 
     def save_checkpoint(self, state, is_best):
@@ -385,24 +303,24 @@ class Trainer:
         If this model has reached the best validation accuracy thus
         far, a separate file with the suffix `best` is created.
         """
-        filename = self.model_name + "_ckpt.pth.tar"
-        ckpt_path = os.path.join(self.ckpt_dir, filename)
+        filename = self.config.model_name + "_ckpt.pth.tar"
+        ckpt_path = os.path.join(self.config.ckpt_dir, filename)
         torch.save(state, ckpt_path)
         if is_best:
-            filename = self.model_name + "_model_best.pth.tar"
-            shutil.copyfile(ckpt_path, os.path.join(self.ckpt_dir, filename))
+            filename = self.config.model_name + "_model_best.pth.tar"
+            shutil.copyfile(ckpt_path, os.path.join(self.config.ckpt_dir, filename))
 
     def load_checkpoint(self, best=False):
         """Load the best copy of a model.
         Args:
             best: if set to True, loads the best model.
         """
-        print("[*] Loading model from {}".format(self.ckpt_dir))
+        print("[*] Loading model from {}".format(self.config.ckpt_dir))
 
-        filename = self.model_name + "_ckpt.pth.tar"
+        filename = self.config.model_name + "_ckpt.pth.tar"
         if best:
-            filename = self.model_name + "_model_best.pth.tar"
-        ckpt_path = os.path.join(self.ckpt_dir, filename)
+            filename = self.config.model_name + "_model_best.pth.tar"
+        ckpt_path = os.path.join(self.config.ckpt_dir, filename)
         ckpt = torch.load(ckpt_path, map_location="cpu")
 
         # load variables from checkpoint
